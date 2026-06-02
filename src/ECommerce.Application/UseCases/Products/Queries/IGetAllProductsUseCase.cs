9ec87885a7fcfb340d1a486de857744015e623a8
Julieta-Laspiur <julieta.laspiur771@gmial.com>
2026-06-02 13:26:54 -0300

    Se remueven las abstracciones directas de los casos de uso

--- src/ECommerce.Application/UseCases/Products/Queries/IGetAllProductsUseCase.cs
+++
@@ -1,8 +0,0 @@
-using ECommerce.Domain.Entities;
-
-namespace ECommerce.Application.UseCases.Products.Queries;
-
-public interface IGetAllProductsUseCase
-{
-    Task<IEnumerable<Product>> ExecuteAsync();
-}
--- src/ECommerce.Application/UseCases/Products/Queries/IGetProductByIdUseCase.cs
+++
@@ -1,8 +0,0 @@
-using ECommerce.Domain.Entities;
-
-namespace ECommerce.Application.UseCases.Products.Queries;
-
-public interface IGetProductByIdUseCase
-{
-    Task<Product?> ExecuteAsync(Guid id, CancellationToken ct = default);
-}
